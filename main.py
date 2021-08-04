920f30e6cdc8f8fc5c6ed2fe65c7edef695e8ac0
benciuf <florin.benciu@inoe.ro>
2021-08-04 16:43:29 +0200

    update 0.1

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ sq_matrix_length = 5
 def return_queen_pos(x,y):
     return x,y
 
+def show_board():
+    for i in range(sq_matrix_length):
+        print(" ",i)
+        for j in range(sq_matrix_length):
+            print(" ")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     pos_x = random.randint(0,sq_matrix_length**2)
     pos_y = random.randint(0,sq_matrix_length**2)
     print("Board level: ", sq_matrix_length**2)
     print(return_queen_pos(pos_x,pos_y))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
