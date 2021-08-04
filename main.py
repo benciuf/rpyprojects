83a7f8c4a62f5df9864f43eae5a62f9a823cdcad
benciuf <florin.benciu@inoe.ro>
2021-08-04 16:55:59 +0200

    update 0.1

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import random
-
+import random as rand
+import numpy as np
 sq_matrix_length = 5
 
 def return_queen_pos(x,y):
     return x,y
 
 def show_board():
-    for i in range(sq_matrix_length):
-        print(" ",i)
-        for j in range(sq_matrix_length):
-            print(" ")
+    board_matrix = np.zeros((sq_matrix_length, sq_matrix_length))
+    print(board_matrix)
+
+def add_queen(x,y):
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pos_x = random.randint(0,sq_matrix_length**2)
-    pos_y = random.randint(0,sq_matrix_length**2)
+    pos_x = rand.randint(0,sq_matrix_length**2)
+    pos_y = rand.randint(0,sq_matrix_length**2)
     print("Board level: ", sq_matrix_length**2)
     print(return_queen_pos(pos_x,pos_y))
+    show_board()
+    add_queen(pos_x,pos_y)
